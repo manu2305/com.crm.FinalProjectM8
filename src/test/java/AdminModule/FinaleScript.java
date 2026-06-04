3d092446fa7788d31d0971fbf00c8ae9ea7f1bb3
shree887 <100765082+shree887@users.noreply.github.com>
2026-06-04 10:57:41 +0530

    bye

---
+++ src/test/java/AdminModule/FinaleScript.java
@@ -0,0 +1,11 @@
+package AdminModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FinaleScript {
+@Test
+public void main() {
+	Reporter.log("Please Follow the Automation principles",true);
+}
+}
